28ab33f87e7f2b41f165a026604b8cdcc09069e3
jacob zlogar <jacob@vermill.io>
2025-01-04 00:20:14 -0500

    Remove that stuff

--- src/main.rs
+++ src/main.rs
@@ -13,23 +13,6 @@ fn main() -> Result<()> {
         let opcode_fn = OPCODE_TABLE[*byte as usize];
         let instruction = (opcode_fn)(&mut iter)?;
         println!("{:?}", instruction);
-        // for opcode in OPCODE_TABLE {
-        //     if (*byte >> opcode.bit_shift) == opcode.mask {
-        //         match opcode.opcode {
-        //             Opcode::Mov(mnemonic) => {
-        //                 let mut op = Mov::new(mnemonic.clone(), byte.clone());
-        //                 op.parse(&mut iter.clone())?;
-        //                 cpu.mov(op.dest.clone(), op.source.clone(), op.value);
-        //                 // println!("{op}");
-        //             }
-        //             Opcode::Arithmetic(mnemonic) => {
-        //                 println!("{:08b}", byte);
-        //                 let next = iter.next().unwrap();
-        //                 println!("{:08b}", next);
-        //             }
-        //         }
-        //     }
-        // }
     }
     Ok(())
 }
